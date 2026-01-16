aa17a12f937a3ac0ec6ebbcc7051ae5e4dd97374
Maurice <115727701+Muggwoffin@users.noreply.github.com>
2026-01-16 13:29:23 +0000

    Update events

--- events.js
+++ events.js
@@ -14,6 +14,17 @@ const events = [
         "badge": "Online",
         "description": "Discussing Hotel Lux as part of the Kennan Institute's Long View series.",
         "link": "https://www.kennaninstitute.org/longview"
+    },
+    {
+        "id": "first-add-oats-then-kill-hitler-a-recipe-for-resistance-in-underground-europe-1920s-1940s",
+        "title": "First Add Oats, Then Kill Hitler: A Recipe for Resistance in Underground Europe, 1920s-1940s",
+        "date": "2026-02-20",
+        "time": "16:00",
+        "timezone": "GMT",
+        "location": "01/003, 27 University Square, QUB",
+        "badge": "In-Person",
+        "description": "A paper on my new project examining the ISK.",
+        "link": null
     }
 ];
 
