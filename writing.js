36e5fde8bc724b4e4edbb16f2800b85ea4530c29
Maurice <115727701+Muggwoffin@users.noreply.github.com>
2026-01-17 14:18:31 +0000

    Update writing

--- writing.js
+++ writing.js
@@ -29,11 +29,11 @@ const writing = [
         "outletLogo": "images/outlet-logo-2.png"
     },
     {
-        "title": "Third Article Title",
-        "outlet": "Outlet Name",
-        "date": "Date Published",
-        "excerpt": "Brief excerpt or summary...",
-        "link": "#",
+        "title": "Test",
+        "outlet": "Test",
+        "date": "Test",
+        "excerpt": "Test",
+        "link": "https://meduza.io/en/feature/2025/03/14/radical-crossroads",
         "outletLogo": "images/outlet-logo-3.png"
     }
 ];
