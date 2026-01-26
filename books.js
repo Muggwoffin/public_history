5bd8c1688044b91fa7dabfd62e1b125c4ccaccfc
Maurice <115727701+Muggwoffin@users.noreply.github.com>
2026-01-26 15:15:49 +0000

    Update books

--- books.js
+++ books.js
@@ -10,7 +10,7 @@ const books = [
         "publisher": "Footnote Press",
         "year": "2024",
         "cover": "images/book-cover-1.png",
-        "description": "Critically acclaimed book uncovering the intimate lives of international communists who lived in Moscow's Hotel Lux during the 1920sâ1940s, revealing forgotten stories of idealism, love, betrayal, and terror. Shortlisted for Irish Book Awards 2024 (History Book of Year).",
+        "description": "Critically acclaimed book uncovering the intimate lives of international communists who lived in Moscow's Hotel Lux during the 1920s and 940s, revealing forgotten stories of idealism, love, betrayal, and terror. Shortlisted for Irish Book Awards 2024 (History Book of Year). An Irish Times, Foreign Affairs and Big Issue Book of the Year.",
         "publisherLink": "https://footnotepress.com",
         "reviewsLink": null,
         "bookshopLink": "https://uk.bookshop.org/p/books/hotel-lux-an-intimate-history-of-communism-s-forgotten-radicals-maurice-j-casey/7664562?ean=9781804442227&next=t"
